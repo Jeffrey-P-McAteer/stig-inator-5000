afabf1c0c785132f1e1e985dc8c1efe0f69c31ee
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2024-07-17 13:29:33 -0400

    Added control over dict gen

--- scan_for_not_applicables.py
+++ scan_for_not_applicables.py
@@ -53,33 +53,35 @@ for i in range(0, len(stigs)):
                 for j in range(i, 0, -1):
                     if rule_text[j:j+3] == ' if':
                         text_fragment = rule_text[j:i+len(end_clause)].strip()
-                        print(text_fragment)
-                        rule_text_fragments.append(text_fragment)
+                        if not text_fragment in rule_text_fragments:
+                            print(text_fragment)
+                            rule_text_fragments.append(text_fragment)
                         break
 
 print(f'============= Generated Desktop-App Match code =============')
-for rule_fragment in rule_text_fragments:
-   if 'is not applicable' in rule_fragment:
-        inverted_fragment = rule_fragment.rsplit(',', maxsplit=1)[0].strip()
-        inverted_fragment = inverted_fragment.removesuffix('the requirement is not applicable')
-        inverted_fragment = inverted_fragment.removesuffix('in this case')
-        inverted_fragment = inverted_fragment.removesuffix(' ')
-        inverted_fragment = inverted_fragment.removesuffix(' ')
-        inverted_fragment = inverted_fragment.removesuffix('.')
-        inverted_fragment = inverted_fragment.removeprefix('if ')
+if 'y' in input('Generate? ').lower():
+    for rule_fragment in rule_text_fragments[:1]:
+        if 'is not applicable' in rule_fragment:
+            inverted_fragment = rule_fragment.rsplit(',', maxsplit=1)[0].strip()
+            inverted_fragment = inverted_fragment.removesuffix('the requirement is not applicable')
+            inverted_fragment = inverted_fragment.removesuffix('in this case')
+            inverted_fragment = inverted_fragment.removesuffix(' ')
+            inverted_fragment = inverted_fragment.removesuffix(' ')
+            inverted_fragment = inverted_fragment.removesuffix('.')
+            inverted_fragment = inverted_fragment.removeprefix('if ')
 
-        comment_text = inverted_fragment
-        if 'the application' in inverted_fragment:
-           comment_text = inverted_fragment.replace('the application', '{tool_name}')
-           comment_text = comment_text.replace('{tool_name} under review', 'the application under review')
-           comment_text = comment_text.replace('{tool_name}s', 'the applications')
+            comment_text = inverted_fragment
+            if 'the application' in inverted_fragment:
+                comment_text = inverted_fragment.replace('the application', '{tool_name}')
+                comment_text = comment_text.replace('{tool_name} under review', 'the application under review')
+                comment_text = comment_text.replace('{tool_name}s', 'the applications')
 
-        elif '' in inverted_fragment:
-           pass
+            elif '' in inverted_fragment:
+                pass
 
-        print(f'{{')
-        print(f"  'match': ['{rule_fragment}'],")
-        print(f"  'status': 'not_applicable',")
-        print(f"  'comments': f'{comment_text}',")
-        print(f'}}')
+            print(f'{{')
+            print(f"  'match': ['{rule_fragment}'],")
+            print(f"  'status': 'not_applicable',")
+            print(f"  'comments': f'{comment_text}',")
+            print(f'}}')
 
